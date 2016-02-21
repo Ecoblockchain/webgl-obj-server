141e9651073831b4fa04403f9eb3a57686828a75
Anders Hoff <inconvergent@gmail.com>
2016-02-21 12:56:15 +0100

    install models dir

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,12 +4,16 @@ here=`pwd`
 root=$here/../
 
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s $root/conf/nginx.conf /etc/nginx/sites-enabled/default 
+sudo ln -s $root/conf/nginx.conf /etc/nginx/sites-enabled/default
 
 sudo rm -rf /usr/share/nginx/www
 sudo mkdir -p /usr/share/nginx/www
 sudo cp $root/www/* /usr/share/nginx/www -r
 
-cd $here
+sudo rm -rf /usr/share/nginx/www/models
+mkdir -p $here/../../models
+sudo chmod 755 $here/../../models
+sudo ln -s $here/../../models /usr/share/nginx/www/models
 
+cd $here
 
